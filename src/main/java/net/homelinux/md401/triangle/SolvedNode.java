8c533a61fc9b82fe41e23b78370c72150f96fb59
Michael Donaghy <md401@srcf.ucam.org>
2013-01-29 16:59:54 +0000

    Some kind of a path

--- src/main/java/net/homelinux/md401/triangle/SolvedNode.java
+++ src/main/java/net/homelinux/md401/triangle/SolvedNode.java
@@ -1,15 +1,40 @@
 package net.homelinux.md401.triangle;
 
-class SolvedNode {
-	final int index;
-	final int cost;
-	final int costAfter;
-	final int nextIndex;
+import org.apache.commons.lang.StringUtils;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
+
+interface ISolvedNode {
 	
-	SolvedNode(int index, int cost, int costAfter, int nextIndex) {
-		this.index = index;
-		this.cost = cost;
-		this.costAfter = costAfter;
-		this.nextIndex = nextIndex;
+	String toDescription(ImmutableList<Integer> pathUpToHere);
+
+	static class SolvedNode implements ISolvedNode {
+		final int cost;
+		final int totalFromHereOn;
+		final ISolvedNode next;
+		public SolvedNode(int cost, int totalFromHereOn, ISolvedNode next) {
+			this.cost = cost;
+			this.totalFromHereOn = totalFromHereOn;
+			this.next = next;
+		}
+		
+		public String toDescription(ImmutableList<Integer> pathUpToHere) {
+			Builder<Integer> path = ImmutableList.builder();
+			path.addAll(pathUpToHere);
+			path.add(cost);
+			return next.toDescription(path.build());
+		}
+	}
+
+	static class EndNode implements ISolvedNode {
+		public String toDescription(ImmutableList<Integer> pathUpToHere) {
+			// XXX: is there really no sum function in Java? I've been
+			// writing scala too long
+			int total = 0;
+			for (int i : pathUpToHere)
+				total += i;
+			return StringUtils.join(pathUpToHere, " + ") + " = " + total;
+		}
 	}
 }
