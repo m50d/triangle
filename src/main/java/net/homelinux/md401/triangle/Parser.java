5880bc3066507e9763f6225b27f5de112f65137f
Michael Donaghy <md401@srcf.ucam.org>
2013-01-29 17:25:44 +0000

    Finally figure out what library we want

--- src/main/java/net/homelinux/md401/triangle/Parser.java
+++ src/main/java/net/homelinux/md401/triangle/Parser.java
@@ -1,5 +1,18 @@
 package net.homelinux.md401.triangle;
 
-class Parser {
+import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
+
+public abstract class Parser {
+	public Parser(final InputStream is) {
+		try {
+			//Yes, we really do want to use the system default encoding, because we're reading from stdin
+			IOUtils.readLines(is);
+		} catch (IOException e) {
+			//If we get an error reading from stdin that's almost certainly unrecoverable
+			throw new RuntimeException(e);
+		}
+	}
 }
